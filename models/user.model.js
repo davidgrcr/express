630c50b634cb04ca0784fcc071caf547268bfaeb
David <davidgrcr@gmail.com>
2015-08-22 21:25:06 +0200

    Crea metodo para saber si un ususario es admin y lo emplea en el menu

--- models/user.model.js
+++ models/user.model.js
@@ -1,30 +1,35 @@
 var mongoose = require('mongoose'),
-Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	username: {
-		type: String,
-		required : true,
-		unique: true
-	},	
-	email: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required : true
-	},
-	rol: {
-		type: String,
-		enum: ['cliente', 'admin'],
-		default: 'cliente'
-	}
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    rol: {
+        type: String,
+        enum: ['cliente', 'admin'],
+        default: 'cliente'
+    }
 });
 
 //  Método estatico para buscar por username
-userSchema.statics.findByUserName = function(username, cb) {	
-	this.model('user').findOne({username: username}, cb);	
+userSchema.statics.findByUserName = function (username, cb) {
+    this.model('user').findOne({username: username}, cb);
 };
+
+userSchema.methods.isAdmin = function () {
+    return (this.rol == 'admin');
+    // `this` would be [property]
+}
 
 module.exports = mongoose.model('user', userSchema);
