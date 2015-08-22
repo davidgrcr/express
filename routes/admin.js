1f60b1a28bb9aa5b65f3f8ead8fbf30f0410cd60
David <davidgrcr@gmail.com>
2015-08-22 19:42:39 +0200

    Modificaciones estructurales

--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,8 @@ var transporter = nodemailer.createTransport();
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: 'davidgrcr@gmail.com',
-        pass: '84778477'
+        user: 'david.gras85@gmail.com',
+        pass: 'developer!'
     }
 });
 
@@ -21,42 +21,44 @@ var mailOptions = {
     html: '<b>Hello world ✔</b>' // html body
 };
 
-router.get('/', function(req, res) {
-	Order.find({})
-	.populate('customer')
-	.exec(function(err, orders){
-		if (err) throw err;
-		res.render('admin',{orders: orders});
-	});
-  
+router.get('/', function (req, res) {
+    Order.find({})
+        .populate('customer')
+        .exec(function (err, orders) {
+            if (err) throw err;
+            res.render('admin', {orders: orders});
+        });
+
 });
 
-router.get('/order/:order/:state', function(req, res) {
-	Order.update({_id:req.params.order}, {"$set":{state:req.params.state}})
-	.exec(function(err, updated){
-		if (err) throw err;
+router.get('/order/:order/:state', function (req, res) {
+    Order.update({_id: req.params.order}, {"$set": {state: req.params.state}})
+        .exec(function (err, updated) {
+            if (err) throw err;
 
-		if(req.params.state == "ready") {
-			Order.findOne({_id: req.params.order})
-			.populate('customer')
-			.exec(function(err, order){
-				if(err) throw err;
-				if(order.email_on_ready){
-					// Mandamos el mail
-					mailOptions.to = order.customer.email;
-					transporter.sendMail(mailOptions, function(err, info){
-						if (err) throw err;
-						console.log("Info email: ", info);
-					});
-				} else {
-					console.log("El usuario no tiene notificacion por email");
-				}
-			});
-			
-		}
-		res.redirect('/admin');
-	});
-  
+            if (req.params.state == "ready") {
+                Order.findOne({_id: req.params.order})
+                    .populate('customer')
+                    .exec(function (err, order) {
+                        if (err) throw err;
+                        if (order.email_on_ready) {
+                            // Mandamos el mail
+                            mailOptions.to = order.customer.email;
+                            transporter.sendMail(mailOptions, function (err, info) {
+                                //if (err) throw err;
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log("Info email: ", info);
+                                }
+                            });
+                        } else {
+                            console.log("El usuario no tiene notificacion por email");
+                        }
+                    });
+            }
+            res.redirect('/admin');
+        });
 });
 
 module.exports = router;
